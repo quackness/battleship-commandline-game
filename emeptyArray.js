bc18ac11b5451f8d37130e232ce0d92d3e789644
Karolina Redden <reddenkarolina@gmail.com>
2023-10-12 14:16:23 +0000

    add code from the teacher

--- emeptyArray.js
+++ emeptyArray.js
@@ -1,13 +1,17 @@
 //generate 3x3 2 darray away with nothing in the values
 
-const myArray = []
-const myArray2 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+// const myArray = []
+const ticTacToe = [['-','-','-'],
+                  ['-','-','-'],
+                  ['-','-','-']];
+ticTacToe[2][2] = 'X'
+ticTacToe[0][2] = 'O'
 
-
-myArray2[2][2] = 'x';
-myArray2[0][2] = 'o';
-console.log(myArray2);
+for(let i=0; i<ticTacToe.length; i++){
+    let row = '';
+    for(let j=0; j<ticTacToe[i].length; j++){
+        //create a visual of each row and print to the screen
+        row += ticTacToe[i][j] + ' '
+    }
+    console.log(row)
+}
