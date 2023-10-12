9478adacd2c06db299467a1b462b67cb2928ffb0
Karolina Redden <reddenkarolina@gmail.com>
2023-10-12 14:08:19 +0000

    start playing the tic tac toe

---
+++ emeptyArray.js
@@ -0,0 +1,13 @@
+//generate 3x3 2 darray away with nothing in the values
+
+const myArray = []
+const myArray2 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
+
+myArray2[2][2] = 'x';
+myArray2[0][2] = 'o';
+console.log(myArray2);
