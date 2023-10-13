62d22211e8a3a40f9ea9655140e8d596a3df1b95
Karolina Redden <reddenkarolina@gmail.com>
2023-10-13 15:23:54 +0000

    add reversing and casting to num

--- battle.js
+++ battle.js
@@ -7,12 +7,6 @@ let readlineSync = require('readline-sync');
 const fs = require('fs');
 let read = fs.readFileSync(__dirname + '/map.txt', 'utf8');
 console.log("raw file", read);
-//create a grid, an outline should be letters and numbers
-//inner grid inside should be the file content
-//ask the user for target
-//verify if the ship is there
-//deduct from the shots
-//dipslay the message
 
 //split by \r and \n
  let splitReadfile = read.split('\r\n');
@@ -27,17 +21,39 @@ console.log("raw file", read);
  console.log(grid);
 
 //create a game board
-const battleBoard = Array.from({length: 10}, e => Array(10).fill(null));
+const battleBoard = Array.from({length: 10}, e => Array(10).fill(" "));
 console.log("battleBoard", battleBoard);
 //ask for input
 let shoot = readlineSync.question("Choose your target (Ex. A1): ")
-console.log(shoot)
+// console.log(shoot)
 let coordinates = shoot.split('');
-console.log(coordinates);
+// console.log(coordinates);
 // console.log(shootArray);
 //reverse the input
 let reversed = coordinates.reverse();
+console.log("reversed", reversed);
+let letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+let extractedLetterCoordinate = letters.indexOf(reversed[1]);
+console.log(extractedLetterCoordinate);
+console.log("reversed2", reversed);
+reversed.splice(1, 1, extractedLetterCoordinate)
+console.log("fin", reversed);
+let castTonumber = (Number(reversed[0]));
+reversed.splice(0, 1, castTonumber)
 console.log(reversed);
+
+for(let i = 0; i<battleBoard.length; i++){
+    let row = '';
+    for(let j=0; j<battleBoard[i].length; j++){
+        //create a visual of each row and print to the screen
+        if (battleBoard[i][j] != " ") {
+            row += battleBoard[i][j] + ' '
+        } else {
+            row += ' _ '
+        }
+    }
+    console.log(row)
+}
 
 
 
