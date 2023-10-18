b68d16dd2be5fa25218b96d6dd9daf7c23eb6a96
Karolina Redden <reddenkarolina@gmail.com>
2023-10-18 18:13:57 +0000

    work in progress

--- battle.js
+++ battle.js
@@ -1,7 +1,7 @@
 //console.log
 console.log("Let's play battleship!");
-console.log("You have 30 misses to fire to sink all fixe ships.");
-let misslesLeft = 5;
+console.log("You have 30 misses to fire to sink all five ships.");
+let misslesLeft = 10;
 const battleBoard = Array.from({ length: 10 }, e => Array(10).fill(" "));
 const board = function (shotResult) {
     if (shotResult !== undefined) {
@@ -26,7 +26,7 @@ board();
 //read the file
 let readlineSync = require('readline-sync');
 const fs = require('fs');
-let read = fs.readFileSync(__dirname + '/map.txt', 'utf8');
+let read = fs.readFileSync(__dirname + '/copy.txt', 'utf8');
 
 //split by \r and \n
 let splitReadfile = read.split('\r\n');
@@ -40,11 +40,11 @@ for (let i = 0; i < splitReadfile.length; i++) {
 }
 
 let countX = 0;
-console.log("grid", grid);
-for (let h = 0; h < grid.length; h++){
+// console.log("grid", grid);
+for (let h = 0; h < grid.length; h++) {
     // console.log(">>", grid[h]);
-    for(let o = 0; o < grid[h].length; o++) {
-        console.log(">>", grid[h][o]);
+    for (let o = 0; o < grid[h].length; o++) {
+        // console.log(">>", grid[h][o]);
         if (grid[h][o] === "1") {
             countX++;
         }
@@ -52,66 +52,55 @@ for (let h = 0; h < grid.length; h++){
 }
 
 
-
-
-// console.log(grid);
-//create a game board
-// const battleBoard = Array.from({ length: 10 }, e => Array(10).fill(" "));
-// console.log("battleBoard", battleBoard);
-// let misslesLeft = 5;
 let hit = 0;
 let miss = 0;
-// shotResult? "HIT!!!" : "Miss";
-// console.log(shotResult);
-
-// function status(value) {
-//     console.log(shotResult? "HIT!!!" : "Miss");
-//   }
 let shotResult;
 
+
+
 while (misslesLeft >= 1) {
-    //ask for input
+    let shoot = readlineSync.question("Choose your target (Ex. A1): ");
+    if (shoot !== undefined) {
+        //ask for input
 
-    let shoot = readlineSync.question("Choose your target (Ex. A1): ")
+        // misslesLeft = misslesLeft - 1;
+        // console.log(`You have ${misslesLeft} missiles left.`);
+        // console.log(shoot)
+        let coordinates = [];
+        let letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+        let extractedNumber = shoot.slice(1);
+        // console.log(extractedNumber);
+        let numberCoordinates = parseInt(extractedNumber);
 
+        coordinates[0] = numberCoordinates;
+        let extractedLetterCoordinate = letters.indexOf(shoot.charAt(0).toLowerCase());
+        coordinates[1] = extractedLetterCoordinate;
 
-    // misslesLeft = misslesLeft - 1;
-    // console.log(`You have ${misslesLeft} missiles left.`);
-
-    // console.log(shoot)
-    let coordinates = [];
-    // let extractedLetterCoordinate = shoot.charAt(0);
-    // console.log(coordinates);
-    // console.log(shootArray);
-    //reverse the input
-    // let reversed = coordinates.reverse();
-    // console.log("reversed", reversed);
-    let letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-
-    let extractedNumber = shoot.slice(1);
-    // console.log(extractedNumber);
-    let numberCoordinates = parseInt(extractedNumber);
-
-    coordinates[0] = numberCoordinates;
-    let extractedLetterCoordinate = letters.indexOf(shoot.charAt(0).toLowerCase());
-    coordinates[1] = extractedLetterCoordinate;
-
-    //check what is in the grid array 1 or 0, then apply X or O accordingly
-    if (grid[coordinates[0] - 1][coordinates[1] - 1] == "1") {
-        shotResult = true
-        battleBoard[coordinates[0] - 1][coordinates[1] - 1] = "X"
-        hit++;
-        board(shotResult);
+        //check what is in the grid array 1 or 0, then apply X or O accordingly
+        if (grid[coordinates[0] - 1][coordinates[1] - 1] == "1") {
+            shotResult = true
+            battleBoard[coordinates[0] - 1][coordinates[1] - 1] = "X"
+            hit++;
+            board(shotResult);
+        } else {
+            shotResult = false;
+            battleBoard[coordinates[0] - 1][coordinates[1] - 1] = "O";
+            miss++;
+            board(shotResult);
+        }
+        console.log(countX);
+        if (hit == countX) {
+            console.log(`YOU SANK MY ENTIRE FLEET!\nYou had ${hit} out of ${countX} hits, which sank all the ships.\n You won, congratulations!`);
+            break;
+        }
     } else {
-        shotResult = false;
-        battleBoard[coordinates[0] - 1][coordinates[1] - 1] = "O";
-        miss++;
-        board(shotResult);
+        console.log("Invalid entry")
     }
-
-
 }
-console.log(`You have 0 missiles remaining.\nGAME OVER.\nYou had ${hit} out of 17 hits.\nBetter luck next time.`);
+
+if (misslesLeft == 0) {
+    console.log(`You have 0 missiles remaining.\nGAME OVER.\nYou had ${hit} out of ${countX} hits.\nBetter luck next time.`);
+};
 
 
 
@@ -123,17 +112,7 @@ console.log(`You have 0 missiles remaining.\nGAME OVER.\nYou had ${hit} out of 1
 
 
 
-// const gameplayGrid = [[null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null],
-//                       [null,null,null,null,null,null,null,null,null,null]]
-// console.log(gameplayGrid2)
+
 
 
 
