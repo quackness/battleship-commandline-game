8fcdf0f6bb810e70d31d071fbcd48a1c5588debc
Karolina Redden <reddenkarolina@gmail.com>
2023-10-19 19:12:27 +0000

    add chalk

--- battle.js
+++ battle.js
@@ -1,16 +1,20 @@
+//import chalk
+const chalk = require('chalk');
+
 //console.log
 console.log("Let's play battleship!");
-console.log("You have 30 misses to fire to sink all five ships.");
+console.log('You have ' + chalk.underline('30 missles') + ' to fire to sink all five ships.');
+console.log(" ");
 let misslesLeft = 10;
 const battleBoard = Array.from({ length: 10 }, e => Array(10).fill(" "));
 const board = function (shotResult) {
     if (shotResult !== undefined) {
-        console.log(shotResult ? "HIT!!!" : "Miss")
+        console.log(shotResult ? chalk.green('HIT!!!') : chalk.red('Miss'))
         misslesLeft--;
         console.log(`You have ${misslesLeft} missiles left.`);
     }
 
-    console.log("  A B C D E F G H I J")
+    console.log(chalk.bold.blue("  A B C D E F G H I J"));
     let counter = 0;
     for (let i = 0; i < battleBoard.length; i++) {
         let row = '';
@@ -21,12 +25,12 @@ const board = function (shotResult) {
         }
         console.log(`${counter} ${row}`)
     }
+    console.log("");
 }
 board();
 //read the file
 let readlineSync = require('readline-sync');
 const fs = require('fs');
-const { groupEnd } = require('console');
 let read = fs.readFileSync(__dirname + '/copy.txt', 'utf8');
 
 //split by \r and \n
@@ -56,7 +60,7 @@ let letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
 while (misslesLeft >= 1) {
     let shoot = readlineSync.question("Choose your target (Ex. A1): ");
-    if (shoot === '') {
+    if (shoot === '' || shoot.length === 1 || shoot.length > 3) {
         console.log("Invalid input")
     } else {
         let extractedNumber = shoot.slice(1);
@@ -66,19 +70,17 @@ while (misslesLeft >= 1) {
         let extractedLetterCoordinate = letters.indexOf(shoot.charAt(0).toLowerCase());
         coordinates[1] = extractedLetterCoordinate;
 
-        console.log(coordinates);
-
-        if (coordinates[0] > grid.length || coordinates[1] > grid[0].length || coordinates[0] === NaN || coordinates[1] === -1) {
+        if (coordinates[0] > grid.length || coordinates[1] > grid[0].length || isNaN(numberCoordinates) || coordinates[1] === -1 ) {
             console.log("Invalid input")
         }
         else if (grid[coordinates[0] - 1][coordinates[1] - 1] == "1") {
             shotResult = true
-            battleBoard[coordinates[0] - 1][coordinates[1] - 1] = "X"
+            battleBoard[coordinates[0] - 1][coordinates[1] - 1] = chalk.bgGreen("X")
             hit++;
             board(shotResult);
         } else {
             shotResult = false;
-            battleBoard[coordinates[0] - 1][coordinates[1] - 1] = "O";
+            battleBoard[coordinates[0] - 1][coordinates[1] - 1] = chalk.red("O");
             miss++;
             board(shotResult);
         }
